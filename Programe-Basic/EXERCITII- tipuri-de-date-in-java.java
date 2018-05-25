7e82e10db656930a8670e25f622ffabb3e0caf58
Bogdan Pașca <bogdan_hunt2003@yahoo.com>
2018-05-25 13:32:41 +0300

    Update EXERCITII- tipuri-de-date-in-java.java

--- Programe-Basic/EXERCITII- tipuri-de-date-in-java.java
+++ Programe-Basic/EXERCITII- tipuri-de-date-in-java.java
@@ -1,1 +1,27 @@
-.
+EXERCITIU 1
+  
+  
+byte var1 = 20;
+	short var2 = 300;
+	int var3 = 1000;
+  long var4 = 50000 + 10 * (var1 + var2 + var3);
+        System.out.println(var4);
+
+
+EXERCITIU 2
+  
+
+public class Main {
+
+    public static void main(String[] args) {
+        int pounds = 200;
+        double kilograme = 200 * 0.45359237;
+        System.out.println(kilograme);
+    }
+}
+
+  
+  
+
+
+https://programare.bogdanpasca.ro/tipuri-de-date-in-java-exercitii/
